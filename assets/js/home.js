3045f0a8b329c48b3be991ac987efd01d84a11e1
vitorino josé <vitorinojoseneto@gmail.com>
2024-01-30 16:39:53 +0100

    acabei de criar a section do form então está tudo bem até agora

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,12 @@
 function stopped() {
     alert("Crie conta primeiro")
   }
+  function getin() {
+    var x = document.getElementById("login");
+    if(x.style.display == "none") {
+                 x.style.display = "block";
+             }
+          else {
+                 x.style.display = "none";
+  }
+}
