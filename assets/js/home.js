1df31bb3c78316ca269558dfbd1ab2ff8702f9da
vitorino josé <vitorinojoseneto@gmail.com>
2024-02-02 13:47:24 +0100

    troquei o bg das section de cadastro

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,3 +10,12 @@ function stopped() {
                  x.style.display = "none";
   }
 }
+  function cadaster() {
+    var x = document.getElementById("cadasting");
+    if(x.style.display == "none") {
+                 x.style.display = "block";
+             }
+          else {
+                 x.style.display = "none";
+  }
+}
